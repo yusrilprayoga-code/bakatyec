5cb6c9eaafc02935ccdd75344530021bcbee043b
rayhannaufal <rayhan.naufal55@gmail.com>
2024-01-02 16:38:35 +0700

    Update navbar n footer

--- src/App.vue
+++ src/App.vue
@@ -1,149 +1,14 @@
 <template>
   <div id="App">
-    <el-menu
-      :default-active="activeIndex2"
-      active-text-color=" #f05326"
-      class="el-menu-demo"
-      mode="horizontal"
-      @select="handleSelect"
-      router="true"
-    >
-      <el-menu-item index="0">
-        <img style="width: 230px" src="./assets/logo.png" alt="logo" />
-      </el-menu-item>
-      <el-menu-item style="width: 31vw"> </el-menu-item>
-      <el-menu-item index="/">
-        <i class="bx bx-home" style="margin-right: 10px"></i>
-        Beranda
-      </el-menu-item>
-      <el-menu-item index="/class-prakerja">
-        <i class="bx bx-building-house" style="margin-right: 10px"></i>
-        Kelas Prakerja
-      </el-menu-item>
-      <el-menu-item index="/privacy">
-        <i class="bx bx-book" style="margin-right: 10px"></i>
-        Privacy
-      </el-menu-item>
-      <el-menu-item index="/class-menu">
-        <i class="bx bx-book-open" style="margin-right: 10px"></i>
-        Kelas Saya
-      </el-menu-item>
-      <el-menu-item index="#notification" @click="notification">
-        <i class='bx bxs-bell'></i>
-      </el-menu-item>
-      <el-menu-item>
-        <el-dropdown :hide-on-click="false">
-    <span class="el-dropdown-link">
-      <i class='bx bxs-user'></i>
-      MIA
-    </span>
-    <template #dropdown>
-      <el-dropdown-menu>
-        <el-dropdown-item>
-          <i class='bx bx-user'></i>
-          Setting Profile
-        </el-dropdown-item>
-        <el-dropdown-item>
-          <i class='bx bx-building-house' ></i>
-          Bimbingan Prakerja
-        </el-dropdown-item>
-        <el-dropdown-item>
-          <i class='bx bx-book' ></i>
-          Subskripsi
-        </el-dropdown-item>
-        <el-dropdown-item>
-          <i class='bx bx-log-out' ></i>
-          Logout
-        </el-dropdown-item>
-      </el-dropdown-menu>
-    </template>
-  </el-dropdown>
-      </el-menu-item>
-    </el-menu>
+    <NavBar />
     <router-view />
-    <el-footer
-    >
-      <el-row :gutter="30" style="justify-content: center; margin-left: 20px;" class="footer-container">
-        <!-- Tentang Kami -->
-        <el-col class="tentangkami" :span="24" :md="8">
-          <img src="./assets/logo.png" alt="Yec" width="50%" />
-          <p style="font-size: 16px">
-            Sertifikasi dan Kelas Bahasa Inggris Online. Mengembangkan kelas
-            bahasa Inggris inklusif untuk semua orang, serta kelas pengembangan
-            diri lainnya
-          </p>
-          <p style="font-size: 16px">
-            Lembaga Pelatihan YEC CO ID juga tergabung dalam program Kartu
-            Prakerja dan berafiliasi dengan Digital Platform seperti Karier.mu,
-            Tokopedia, Pintar, Pijar Mahir, dan Bukalapak.
-          </p>
-        </el-col>
-
-        <!-- Kontak Kami -->
-        <el-col :span="24" :md="8" class="alamat">
-          <h4 style="font-weight: bold; font-size: 20px;">Alamat</h4>
-          <p style="font-size: 16px">
-            Jl. Nogotirto no.14 Modinan Banyuraden Gamping Sleman Yogyakarta
-            55293
-          </p>
-        </el-col>
-
-        <!-- Ikuti Kami -->
-        <el-col :span="24" :md="8" class="customerService">
-          <h4 style="font-weight: bold; font-size: 20px;">Customer Service</h4>
-          <el-row>
-            <el-col :span="3">
-              <a href="https://www.facebook.com/yec.co.id" target="_blank">
-                <i class="bx bxl-facebook-square" style="font-size: 30px"></i>
-              </a>
-            </el-col>
-            <el-col :span="3">
-              <a href="https://www.instagram.com/yec.co.id/" target="_blank">
-                <i class="bx bxl-instagram-alt" style="font-size: 30px"></i>
-              </a>
-            </el-col>
-            <el-col :span="3">
-              <a
-                href="https://www.youtube.com/channel/UCZ9Z8QqZJ8Q4Z9Z8QqZJ8Q4w"
-                target="_blank"
-              >
-                <i class="bx bxl-youtube" style="font-size: 30px"></i>
-              </a>
-            </el-col>
-            <el-col :span="3">
-              <a href="https://t.me/yec_co_id" target="_blank">
-                <i class="bx bxl-telegram" style="font-size: 30px"></i>
-              </a>
-            </el-col>
-          </el-row>
-          <p style="font-size: 16px">
-            Email : prakerja@yec.co.id
-          </p >
-          <p style="font-size: 16px">
-            CS: +62 853-3584-9962 (WhatsApp Chat)
-          </p>
-        </el-col>
-      </el-row>
-    </el-footer>
+    <Footer/>
   </div>
 </template>
 
-<script lang="ts" setup>
-import { ref } from "vue";
-import { ElNotification, ElDropdown } from "element-plus";
-const activeIndex2 = ref("1");
-const handleSelect = (key: string, keyPath: string[]) => {
-  console.log(key, keyPath);
-};
-
-const notification = () => {
-  ElNotification({
-    title: "Notifikasi",
-    message: "Fitur ini masih dalam pengembangan",
-    type: "warning",
-  });
-};
-
+<script setup lang="ts">
+  import NavBar from './components/NavBar.vue';
+  import Footer from './components/Footer.vue';
 </script>
 
 <style>
@@ -155,7 +20,7 @@ const notification = () => {
   background-color: #eff2f7;
 }
 
-body{
+body {
   margin: 0;
 }
 
@@ -192,18 +57,18 @@ body{
 .alamat {
   text-align: left;
   flex: 1;
-  max-width: 100%; 
+  max-width: 100%;
 }
 
 .tentangkami {
   text-align: left;
   flex: 1;
-  max-width: 100%; 
+  max-width: 100%;
 }
 
 .customerService {
   text-align: left;
   flex: 1;
-  max-width: 100%; 
+  max-width: 100%;
 }
 </style>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,64 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-    <el-footer>
-        <p>Footer</p>
+     <el-footer>
+      <el-row :gutter="30" style="justify-content: center; margin-left: 20px;" class="footer-container">
+        <!-- Tentang Kami -->
+        <el-col class="tentangkami" :span="24" :md="8">
+          <img src="../assets/logo.png" alt="Yec" width="50%" />
+          <p style="font-size: 16px">
+            Sertifikasi dan Kelas Bahasa Inggris Online. Mengembangkan kelas
+            bahasa Inggris inklusif untuk semua orang, serta kelas pengembangan
+            diri lainnya
+          </p>
+          <p style="font-size: 16px">
+            Lembaga Pelatihan YEC CO ID juga tergabung dalam program Kartu
+            Prakerja dan berafiliasi dengan Digital Platform seperti Karier.mu,
+            Tokopedia, Pintar, Pijar Mahir, dan Bukalapak.
+          </p>
+        </el-col>
+
+        <!-- Kontak Kami -->
+        <el-col :span="24" :md="8" class="alamat">
+          <h4 style="font-weight: bold; font-size: 20px;">Alamat</h4>
+          <p style="font-size: 16px">
+            Jl. Nogotirto no.14 Modinan Banyuraden Gamping Sleman Yogyakarta
+            55293
+          </p>
+        </el-col>
+
+        <!-- Ikuti Kami -->
+        <el-col :span="24" :md="8" class="customerService">
+          <h4 style="font-weight: bold; font-size: 20px;">Customer Service</h4>
+          <el-row>
+            <el-col :span="3">
+              <a href="https://www.facebook.com/yec.co.id" target="_blank">
+                <i class="bx bxl-facebook-square" style="font-size: 30px"></i>
+              </a>
+            </el-col>
+            <el-col :span="3">
+              <a href="https://www.instagram.com/yec.co.id/" target="_blank">
+                <i class="bx bxl-instagram-alt" style="font-size: 30px"></i>
+              </a>
+            </el-col>
+            <el-col :span="3">
+              <a href="https://www.youtube.com/channel/UCZ9Z8QqZJ8Q4Z9Z8QqZJ8Q4w" target="_blank">
+                <i class="bx bxl-youtube" style="font-size: 30px"></i>
+              </a>
+            </el-col>
+            <el-col :span="3">
+              <a href="https://t.me/yec_co_id" target="_blank">
+                <i class="bx bxl-telegram" style="font-size: 30px"></i>
+              </a>
+            </el-col>
+          </el-row>
+          <p style="font-size: 16px">
+            Email : prakerja@yec.co.id
+          </p>
+          <p style="font-size: 16px">
+            CS: +62 853-3584-9962 (WhatsApp Chat)
+          </p>
+        </el-col>
+      </el-row>
     </el-footer>
 </template>
 
