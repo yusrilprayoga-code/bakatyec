d494e4b2edfe8475ce41b7fa81c53758cd1bb971
rayhannaufal <rayhan.naufal55@gmail.com>
2024-01-19 23:23:30 +0700

    memperbaiki component sub activity

--- src/components/ActivityDetailClass.vue
+++ src/components/ActivityDetailClass.vue
@@ -13,9 +13,12 @@
                   </h4>
                 </template>
                 <hr />
-                <div @click="openActivity(0)" class="activityContent">
+                <div 
+                  class="activityContent" v-for="(subActivityItem, index) in activityDetail.activityDetails"
+                  :key="subActivityItem.subActivityId"
+                  @click.prevent="!(subActivityItem.progress[0]) ? addProgress(subActivityItem.subActivityId) : ''">
                   <div class="detail">
-                    <div class="checkIcon noDisplay" style="width: 20px">
+                    <div class="checkIcon" v-show="(subActivityItem.progress[0])" style="width: 20px">
                       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024" data-v-ea893728="" width="15">
                         <path fill="#67C23A"
                           d="M512 64a448 448 0 1 1 0 896 448 448 0 0 1 0-896m-55.808 536.384-99.52-99.584a38.4 38.4 0 1 0-54.336 54.336l126.72 126.72a38.272 38.272 0 0 0 54.336 0l262.4-262.464a38.4 38.4 0 1 0-54.272-54.336z">
@@ -23,48 +26,37 @@
                       </svg>
                     </div>
                     <div>
-                      <div style="display: flex; flex-direction: column; cursor: pointer;"
-                        v-for="(subActivityItem, index) in activityDetail.activityDetails"
-                        :key="subActivityItem.subActivityId"
-                        @click="!(subActivityItem.progress[0]) ? addProgress(subActivityItem.subActivityId) : ''">
-                        <div>
-                          <h4 style="margin: 0">
-                            {{ index + 1 }}. {{ subActivityItem.subActivityName }}
-                            <svg v-if="(subActivityItem.progress[0]) || cliked" xmlns="http://www.w3.org/2000/svg"
-                              viewBox="0 0 1024 1024" width="15" height="15">
-                              <path fill="#67C23A"
-                                d="M512 64a448 448 0 1 1 0 896 448 448 0 0 1 0-896m-55.808 536.384-99.52-99.584a38.4 38.4 0 1 0-54.336 54.336l126.72 126.72a38.272 38.272 0 0 0 54.336 0l262.4-262.464a38.4 38.4 0 1 0-54.272-54.336z">
+                      <div style="display: flex; flex-direction: column; cursor: pointer;">
+                        <h4 style="margin: 0">
+                          {{ index + 1 }}. {{ subActivityItem.subActivityName }}
+                        </h4>
+                        <div class="detail">
+                          <div class="icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024" width="15" height="15">
+                              <path fill="#909399"
+                                d="M160 160v704h704V160zm-32-64h768a32 32 0 0 1 32 32v768a32 32 0 0 1-32 32H128a32 32 0 0 1-32-32V128a32 32 0 0 1 32-32">
+                              </path>
+                              <path fill="#909399"
+                                d="M320 288V128h64v352h256V128h64v160h160v64H704v128h160v64H704v128h160v64H704v160h-64V544H384v352h-64V736H128v-64h192V544H128v-64h192V352H128v-64z">
                               </path>
                             </svg>
-                          </h4>
-                          <div class="detail">
-                            <div class="icon">
-                              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024" width="15" height="15">
-                                <path fill="#909399"
-                                  d="M160 160v704h704V160zm-32-64h768a32 32 0 0 1 32 32v768a32 32 0 0 1-32 32H128a32 32 0 0 1-32-32V128a32 32 0 0 1 32-32">
-                                </path>
-                                <path fill="#909399"
-                                  d="M320 288V128h64v352h256V128h64v160h160v64H704v128h160v64H704v128h160v64H704v160h-64V544H384v352h-64V736H128v-64h192V544H128v-64h192V352H128v-64z">
-                                </path>
-                              </svg>
-                              <p style="margin: 0 5px; color: #909399">
-                                {{ subActivityItem.subActvityType }}
-                              </p>
-                            </div>
-                            <div class="icon">
-                              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024" width="15" height="15">
-                                <path fill="#909399"
-                                  d="M512 896a320 320 0 1 0 0-640 320 320 0 0 0 0 640m0 64a384 384 0 1 1 0-768 384 384 0 0 1 0 768">
-                                </path>
-                                <path fill="#909399"
-                                  d="M512 320a32 32 0 0 1 32 32l-.512 224a32 32 0 1 1-64 0L480 352a32 32 0 0 1 32-32">
-                                </path>
-                                <path fill="#909399"
-                                  d="M448 576a64 64 0 1 0 128 0 64 64 0 1 0-128 0m96-448v128h-64V128h-96a32 32 0 0 1 0-64h256a32 32 0 1 1 0 64z">
-                                </path>
-                              </svg>
-                              <p style="margin: 0 5px; color: #909399">00:02:02</p>
-                            </div>
+                            <p style="margin: 0 5px; color: #909399">
+                              {{ subActivityItem.subActvityType }}
+                            </p>
+                          </div>
+                          <div class="icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024" width="15" height="15">
+                              <path fill="#909399"
+                                d="M512 896a320 320 0 1 0 0-640 320 320 0 0 0 0 640m0 64a384 384 0 1 1 0-768 384 384 0 0 1 0 768">
+                              </path>
+                              <path fill="#909399"
+                                d="M512 320a32 32 0 0 1 32 32l-.512 224a32 32 0 1 1-64 0L480 352a32 32 0 0 1 32-32">
+                              </path>
+                              <path fill="#909399"
+                                d="M448 576a64 64 0 1 0 128 0 64 64 0 1 0-128 0m96-448v128h-64V128h-96a32 32 0 0 1 0-64h256a32 32 0 1 1 0 64z">
+                              </path>
+                            </svg>
+                            <p style="margin: 0 5px; color: #909399">00:02:02</p>
                           </div>
                         </div>
                       </div>
