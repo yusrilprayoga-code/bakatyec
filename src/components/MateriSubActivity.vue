d41ebd3892c72f3fe93f58fcfac50ce1a38e052e
MohYusrilprayoga <124210006@student.upnyk.ac.id>
2024-01-21 20:00:36 +0700

    Update Upload Tugas

--- src/components/MateriSubActivity.vue
+++ src/components/MateriSubActivity.vue
@@ -1,40 +1,89 @@
 <template>
-    <div class="boxContent">
-      <!-- <h2>{{ param }}</h2> -->
-      <div v-for="activityDetail in activity" :key="activityDetail.id">
-        <div v-for="(subActivityItem) in activityDetail.activityDetails" :key="subActivityItem.subActivityId">
-          <div v-if="subActivityItem.subActvityType == 'Menonton Video' && subActivityItem.subActivityId == param">
-            <iframe width="100%" style="height:30vw" src="https://www.youtube.com/embed/RunuGmKxwCU?si=UoTb7sFXGWFxLmNl" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-          </div>
-          <div v-if="subActivityItem.subActvityType == 'Membaca E-Book' && subActivityItem.subActivityId == param">
-            <h1>Membaca Ebook</h1>
-          </div>
-          <div v-if="subActivityItem.subActvityType == 'Mengerjakan Tugas' && subActivityItem.subActivityId == param">
-            <h1>Mengerjakan Tugas</h1>
-          </div>
-          <!-- <h2 v-if="subActivityItem.subActivityId == param" >{{ subActivityItem.subActvityType }}</h2> -->
+  <div class="boxContent">
+    <!-- <h2>{{ param }}</h2> -->
+    <div v-for="activityDetail in activity" :key="activityDetail.id">
+      <div
+        v-for="subActivityItem in activityDetail.activityDetails"
+        :key="subActivityItem.subActivityId"
+      >
+        <div
+          v-if="
+            subActivityItem.subActvityType == 'Menonton Video' &&
+            subActivityItem.subActivityId == param
+          "
+        >
+          <iframe
+            width="100%"
+            style="height: 30vw"
+            src="https://www.youtube.com/embed/RunuGmKxwCU?si=UoTb7sFXGWFxLmNl"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen
+          ></iframe>
         </div>
-      </div>
-      <div id="1" style="display: none;">
-          <img src="../assets/1.png" width="100%" height="auto" alt="">
-      </div>
-      <div id="2" style="display: none;">
-          <iframe width="100%" style="height:30vw" src="https://www.youtube.com/embed/RunuGmKxwCU?si=UoTb7sFXGWFxLmNl" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <div
+          v-if="
+            subActivityItem.subActvityType == 'Membaca E-Book' &&
+            subActivityItem.subActivityId == param
+          "
+        >
+          <h1>Membaca Ebook</h1>
+        </div>
+        <div
+          v-if="
+            subActivityItem.subActvityType == 'Mengerjakan Tugas' &&
+            subActivityItem.subActivityId == param
+          "
+        >
+          <h1>Mengerjakan Tugas</h1>
+          <div>
+            <el-upload
+              v-model:file-list="fileList"
+              class="upload-demo"
+              action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15"
+              :on-preview="handlePreview"
+              :on-remove="handleRemove"
+              list-type="picture"
+            >
+              <el-button style="padding-right: 100px; padding-left: 100px;" type="primary">Click to upload</el-button>
+              <template #tip>
+                <div class="el-upload__tip">
+                  jpg/png files with a size less than 500kb
+                </div>
+              </template>
+            </el-upload>
+          </div>
+        </div>
+        <!-- <h2 v-if="subActivityItem.subActivityId == param" >{{ subActivityItem.subActvityType }}</h2> -->
       </div>
     </div>
-
+    <div id="1" style="display: none">
+      <img src="../assets/1.png" width="100%" height="auto" alt="" />
+    </div>
+    <div id="2" style="display: none">
+      <iframe
+        width="100%"
+        style="height: 30vw"
+        src="https://www.youtube.com/embed/RunuGmKxwCU?si=UoTb7sFXGWFxLmNl"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen
+      ></iframe>
+    </div>
+  </div>
 </template>
 <script>
-
 import axios from "axios";
 export default {
-  props: ['param'], 
-  
+  props: ["param"],
+
   data() {
-    return  {
+    return {
       id: this.$route.params.id,
       activity: {},
-    }
+    };
   },
 
   methods: {
@@ -74,10 +123,10 @@ export default {
         console.error("Error fetching user classes:", error);
       }
     },
-    },
+  },
 
   mounted() {
-    this.getMateri()
+    this.getMateri();
     // const jenis = 2;
     // // 1 merupakan id dari type mengerjakan tugas
     // // 2 merupakan id dari type menonton video
@@ -88,6 +137,21 @@ export default {
     //     element.style.display = 'flex';
     //   }
   },
-  
 };
 </script>
+
+<style>
+.boxContent {
+  margin: 0 auto;
+  padding: 20px;
+  background-color: #fff;
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+}
+
+el-upload {
+  display: block;
+  margin-top: 20px;
+}
+
+</style>
